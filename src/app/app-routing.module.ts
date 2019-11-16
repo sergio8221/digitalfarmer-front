fc04c4425ff00c4683eede9984b11bdaf8188b45
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2019-11-16 08:56:48 +0100

    Division of initial design in angular components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//! Components
+import { MainComponent } from './components/main/main.component';
+import { AnimalsComponent } from './components/animals/animals.component';
+import { CropsComponent } from './components/crops/crops.component';
+import { MachineryComponent } from './components/machinery/machinery.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'animals', component: AnimalsComponent },
+  { path: 'crops', component: CropsComponent },
+  { path: 'machinery', component: MachineryComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '**', redirectTo: 'main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { AnimalsComponent } from './components/animals/animals.component';
+import { CropsComponent } from './components/crops/crops.component';
+import { MachineryComponent } from './components/machinery/machinery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AnimalsComponent,
+    CropsComponent,
+    MachineryComponent
   ],
   imports: [
     BrowserModule,
