dfcfa6096f57cb97c78e68ac7873efdd04e7cc9d
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2019-11-22 17:23:05 +0100

    Initial animals component

--- src/app/components/animals/animals.component.ts
+++ src/app/components/animals/animals.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-animals',
   templateUrl: './animals.component.html',
-  styleUrls: ['./animals.component.scss']
+  styleUrls: ['./animals.component.scss'],
+  host: {
+    class: 'app-screen'
+  }
 })
 export class AnimalsComponent implements OnInit {
 
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  host: {
+    class: 'app-screen'
+  }
 })
 export class MainComponent implements OnInit {
 
